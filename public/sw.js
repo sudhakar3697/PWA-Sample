43e2f114eecf85feb252823a69b76f24d44a401a
sudhakar3697 <sudax3@gmail.com>
2020-05-19 01:09:43 +0530

    Update

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,16 @@
-var dataCacheName = 'pwa-test-data-v1';
-var cacheName = 'pwa-test-1';
-var filesToCache = [
+const dataCacheName = 'pwa-test-data';
+const cacheName = 'pwa-test';
+const filesToCache = [
   '/',
   '/index.html',
   '/icon.png',
 ];
 
 //install the sw
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
@@ -18,11 +18,11 @@ self.addEventListener('install', function(e) {
 });
 
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
         if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
@@ -34,11 +34,11 @@ self.addEventListener('activate', function(e) {
 });
 
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   console.log('[Service Worker] Fetch', e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
 });
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
+
 app.use(express.static('public'));
 
-app.get('/index.html', function (req, res) {
-  res.sendFile(path.join(__dirname,"index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.listen(8000, function () {
-  console.log("Example app listening at http://localhost:8000")
-})
+app.listen(7777, function () {
+  console.log('Example app listening at http://localhost:7777')
+});
